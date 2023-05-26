ee4f5340773ecee53faf55c050c721560ad42c6d
Flora025 <2365349265@qq.com>
2023-05-26 16:04:41 +0800

    init and add static component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
+</script>
+
+<template>
+  <div>
+    <Todo />
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <div>
+    <h1>Todos</h1>
+    <!-- TODO: 增加click事件 -->
+    <button class="clear">Clear completed todos</button>
+
+    <div class="prog">
+      <!-- TODO: 响应式-->
+      <progress max="100" value="70">70%</progress>
+      <p>
+        <!-- TODO: fill with variable-->
+        <b>someNum</b> out of <b> total num of todos </b> completed
+      </p>
+    </div>
+
+    <!-- TODO: 遍历生成items -->
+    <ul class="todos">
+      <li class="todo">
+        <!-- TODO: 根据checked状态切换样式 -->
+        <input type="checkbox" name="isCompleted" />
+        <!-- TODO: 对应上面的input + 变量名 -->
+        <label>itemName</label>
+      </li>
+    </ul>
+
+    <!-- TODO: -->
+    <form class="addForm">
+      <!-- 下面的输入框绑定 -->
+      <label for="add">Add todo</label>
+      <div class="sl">
+        <!-- TODO: 绑变量 -->
+        <input type="text" name="add" id="add" />
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style>
+progress {
+  width: 100%;
+}
+
+.prog {
+  margin-bottom: 1.5rem;
+}
+.prog > p {
+  text-align: end;
+  margin-top: 0rem;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  padding: 0.25rem 0;
+  font-size: 1.5rem;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  padding: 1rem;
+  width: 24px;
+  height: 24px;
+  margin-right: 0.5rem;
+}
+
+.clear {
+  margin-bottom: 1.5rem;
+  border: 1px solid hsl(244, 7%, 54%);
+  color: hsl(247, 34%, 85%);
+  border-radius: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 12px;
+  cursor: pointer;
+}
+
+label.completed {
+  color: hsl(245, 20%, 76%);
+  text-decoration: line-through;
+}
+
+.addForm {
+  margin-top: 1rem;
+  border-top: 1px solid #242424;
+  padding: 1rem 0;
+}
+
+.addForm > label {
+  display: block;
+  color: #ccc5d8;
+}
+
+.sl {
+  display: flex;
+  margin-top: 0.25rem;
+}
+.sl > input {
+  border: 1px solid #3f3f3f;
+  padding: 0.75rem 1rem;
+  border-radius: 0.5rem;
+  margin-right: 0.25rem;
+  width: 100%;
+}
+
+.sl > button {
+  border-color: #42d392;
+  background-color: #42d392;
+  padding: 0.75rem 1rem;
+  border-radius: 0.5rem;
+  color: #1a1a1a;
+}
+</style>
